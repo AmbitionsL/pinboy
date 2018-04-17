19782908ed07ccee4de58b1150f89e758a9580de
AmbitionsL <jamesisaacnewtron@gmail.com>
2018-04-16 19:18:41 -0700

    Made some changes to the 'isitgay' command.

--- commands/isitgay/isitgay.js
+++ commands/isitgay/isitgay.js
@@ -1,5 +1,22 @@
 const { Command } = require('discord.js-commando');
 
+var yesNo = Math.floor(Math.random()*2);
+
+var gay = [
+    'gay.',
+    'super-gay.',
+    'super-mega-gay.',
+    'super-mega-ultra-gay.',
+    'double gay.',
+    'triple gay.',
+    'quadruple gay.',
+    'pretty gay.',
+    'a little gay.',
+    'sorta gay.',
+    'kinda gay.',
+    'a full on faggot.'
+];
+
 class IsItGayCommand extends Command
 {
     constructor(client)
@@ -53,20 +70,3 @@ module.exports = IsItGayCommand;
     //'It is, in fact, ',
     //'It is not, in fact, '
 //];
-
-var yesNo = Math.floor(Math.random()*2);
-
-var gay = [
-    'gay.',
-    'super-gay.',
-    'super-mega-gay.',
-    'super-mega-ultra-gay.',
-    'double gay.',
-    'triple gay.',
-    'quadruple gay.',
-    'pretty gay.',
-    'a little gay.',
-    'sorta gay.',
-    'kinda gay.',
-    'a full on faggot.'
-];
