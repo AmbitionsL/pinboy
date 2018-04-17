6ec59d754922ab86317d091566cee06b03053c37
AmbitionsL <34220521+AmbitionsL@users.noreply.github.com>
2018-04-17 15:08:45 -0700

    Update jamey.js
    Changed the name of the array to my ID so it can be pushed to.

--- commands/quotesave/jamey.js
+++ commands/quotesave/jamey.js
@@ -1,4 +1,4 @@
-var jamey = [
+var 167516561328832512 = [
     'I masturbate in public, I\'m exercising my freedom of speech.',
     'Their anuses are too small.  I don\'t want to talk about this anymore.'
 ]
