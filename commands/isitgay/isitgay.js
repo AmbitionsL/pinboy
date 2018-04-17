1f54e9981bdb2f63d0bec5454583a22ebacbd999
AmbitionsL <jamesisaacnewtron@gmail.com>
2018-04-16 18:17:32 -0700

    Made some changes to the 'isitgay' command.

--- commands/isitgay/isitgay.js
+++ commands/isitgay/isitgay.js
@@ -21,15 +21,52 @@ class IsItGayCommand extends Command
         });
     }
 
-    async run(message, { thing })
+    async run(gay, yesNo, message, { thing })
     {
-        if (!thing)
-        {
-            message.say('It is, in fact, gay.' || 'It is not, in fact, gay.')
-        } else {
-            message.say(thing + ' is, in fact, gay.' || thing + ' is not, in fact, gay.');
+        if (yesNo == 1) {
+            if (!thing)
+            {
+                message.say('It is, in fact, ' + gay)
+            } else {
+                message.say(thing + ' is, in fact, ' + gay);
+            }
+        }
+        if (yesNo == 2) {
+            if (!thing)
+            {
+                message.say('It is not, in fact, gay.')
+            } else {
+                message.say(thing + ' is not, in fact, gay.');
+            }
         }
     }
 }
 
 module.exports = IsItGayCommand;
+
+//var thingGay = [
+    //' is, in fact, ',
+    //' is not, in fact, '
+//];
+
+//var noThingGay = [
+    //'It is, in fact, ',
+    //'It is not, in fact, '
+//];
+
+var yesNo = Math.floor(Math.random()*2);
+
+var gay = [
+    'gay.',
+    'super-gay.',
+    'super-mega-gay.',
+    'super-mega-ultra-gay.',
+    'double gay.',
+    'triple gay.',
+    'quadruple gay.',
+    'pretty gay.',
+    'a little gay.',
+    'sorta gay.',
+    'kinda gay.',
+    'a full on faggot.'
+];
