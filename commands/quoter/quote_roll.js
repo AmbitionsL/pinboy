35fe7e31ebdf4c4a5458a4bb5f4825eb534b8af1
AmbitionsL <jamesisaacnewtron@gmail.com>
2018-04-18 16:26:16 -0700

    Fixes to quote commands.

--- commands/quoter/quote_roll.js
+++ commands/quoter/quote_roll.js
@@ -37,7 +37,7 @@ class QuoteRollCommand extends Command
                 message.reply('"' + Flashfigure[Math.floor(Math.random() * Flashfigure.length)] + '" -Brandon')
             }
         } else {
-            message.say('"' + process[userScore][rand] + '" -' + userName);
+            message.say('"' + process[userScore][Math.floor(Math.random() * userScore.length)] + '" -' + userName);
         }
     }
 }
