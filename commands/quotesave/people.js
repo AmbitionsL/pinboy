2b12e41f152f23037a2d598094354a7ea951b5f6
AmbitionsL <34220521+AmbitionsL@users.noreply.github.com>
2018-04-19 11:32:54 -0700

    Update people.js

--- commands/quotesave/people.js
+++ commands/quotesave/people.js
@@ -1,6 +1,6 @@
 const Flashfigure = require(../quotesave/brandon.js);
 const Sticky_Note = require(../quotesave/jamey.js);
 
-var people = {brandon: [Flashfigure], jamey: [Sticky_Note]};
+var people = {Flashfigure: [Flashfigure]} {Sticky_Note: [Sticky_Note]};
 
 module.exports = people;
