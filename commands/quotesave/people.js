a46243dac716628d9af0966f8bb4a6d160163b5b
AmbitionsL <34220521+AmbitionsL@users.noreply.github.com>
2018-04-19 11:46:31 -0700

    Update people.js

--- commands/quotesave/people.js
+++ commands/quotesave/people.js
@@ -1,6 +1,6 @@
 const Flashfigure = require(../quotesave/brandon.js);
 const Sticky_Note = require(../quotesave/jamey.js);
 
-var people = {Flashfigure: [Flashfigure]} {Sticky_Note: [Sticky_Note]};
+var people = {Flashfigure: [Flashfigure[Math.floor(Math.random() * Flashfigure.length)], Sticky_Note: [Sticky_Note[Math.floor(Math.random() * Sticky_Note.length)]};
 
 module.exports = people;
