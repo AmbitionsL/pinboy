944f2783eacf86a6885aa6b133f17ac0210ae690
AmbitionsL <34220521+AmbitionsL@users.noreply.github.com>
2018-04-19 10:22:54 -0700

    Update jamey.js

--- commands/quotesave/jamey.js
+++ commands/quotesave/jamey.js
@@ -1,3 +1,5 @@
 var Sticky_Note = ['I masturbate in public, I\'m exercising my freedom of speech.', 'Their anuses are too small.  I don\'t want to talk about this anymore.']
+var getThis = {array: 'wow'};
 
 module.exports = Sticky_Note;
+module.exports = getThis;
