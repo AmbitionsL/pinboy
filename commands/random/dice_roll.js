b1f341ee7173f2bb72f055439d6251b38a5faad6
AmbitionsL <34220521+AmbitionsL@users.noreply.github.com>
2018-07-21 20:44:35 -0700

    Update dice_roll.js

--- commands/random/dice_roll.js
+++ commands/random/dice_roll.js
@@ -16,7 +16,7 @@ class DiceRollCommand extends commando.Command
     async run(message, args)
     {
         var roll = Math.floor(Math.random() *20) +1;
-        message.reply("you rolled " + roll ".");
+        message.reply("you rolled " + roll + ".");
     }
 }
 
