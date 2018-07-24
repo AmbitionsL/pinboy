53ecfff1bdd225feaafa8f047b44352c8e85c077
AmbitionsL <34220521+AmbitionsL@users.noreply.github.com>
2018-07-24 00:34:06 -0700

    Update dice_roll.js

--- commands/random/dice_roll.js
+++ commands/random/dice_roll.js
@@ -9,14 +9,38 @@ class DiceRollCommand extends commando.Command
             name: 'roll',
             group: 'random',
             memberName: 'roll',
-            description: 'Rolls a die.'
+            description: 'Rolls a die.',
+            args: [
+                {
+                    key: 'rollNumber',
+                    prompt: '',
+                    type: 'int',
+                    default: ''
+                },
+                {
+                    key: 'diceNumber',
+                    prompt: '',
+                    type: 'int',
+                    default: '1'
+                }
+            ]
         }) ;
     }
 
     async run(message, args)
     {
-        var roll = Math.floor(Math.random() *20) +1;
-        message.reply("you rolled " + roll + ".");
+        if (!rollNumber) {
+            var roll = Math.floor(Math.random() * 20) + 1;
+            message.reply("you rolled " + roll + ".");
+        } else {
+            var roll = Math.floor(Math.random() * rollNumber) + 1;
+            double amountOfDie = 0;
+            
+            while (amountOfDie < diceNumber) {
+                message.reply("you rolled " + roll + ".");
+                amountOfDie += 1;
+            }
+        }
     }
 }
 
